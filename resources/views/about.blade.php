3809bdedd278b12fba5175e4aecd8120472083fe
AndyRedbean45 <andyredwin45@gmail.com>
2025-02-05 13:26:34 +0700

    Menambahkan Page About
    Menambahkan page about dan menambahkan route pada web.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('partials.main')
+
+@section('content')
+    <p class="mt-2 text-center fs-1 fw-bold">About Me</p>
+    <p class="text-center fw-bold">Halo, nama saya Andy. Hobi saya adalah bermain futsal</p>
+@endsection
